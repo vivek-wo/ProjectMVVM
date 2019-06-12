3eb1aeef158e631f79d9891357df5421c01baf63
vivek-wo <vivek.wei.wu@gmail.com>
2019-06-12 23:53:33 +0800

    基于MVVM的快速开发框架

---
+++ app/src/main/java/com/vivek/wo/mvvm/sample/MainActivityFragment.kt
@@ -0,0 +1,7 @@
+package com.vivek.wo.mvvm.sample
+
+import android.support.v4.app.Fragment
+
+class MainActivityFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/vivek/wo/mvvm/sample/login/LoginActivity.kt
@@ -0,0 +1,7 @@
+package com.vivek.wo.mvvm.sample.login
+
+import android.support.v7.app.AppCompatActivity
+
+class LoginActivity :AppCompatActivity(){
+
+}
