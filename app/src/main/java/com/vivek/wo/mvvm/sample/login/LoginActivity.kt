04e21755f39a88519d2942564961213db23149c6
vivek-wo <vivek.wei.wu@gmail.com>
2019-06-13 21:01:24 +0800

    基于MVVM的快速开发框架

--- app/src/main/java/com/vivek/wo/mvvm/sample/login/LoginActivity.kt
+++ app/src/main/java/com/vivek/wo/mvvm/sample/login/LoginActivity.kt
@@ -1,7 +1,18 @@
 package com.vivek.wo.mvvm.sample.login
 
+import android.databinding.DataBindingUtil
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.vivek.wo.mvvm.sample.R
+import com.vivek.wo.mvvm.sample.databinding.ActivityLoginBinding
 
-class LoginActivity :AppCompatActivity(){
+class LoginActivity : AppCompatActivity() {
 
+    lateinit var activityLoginBinding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        activityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+    }
 }
---
+++ app/src/main/java/com/vivek/wo/mvvm/sample/login/LoginViewModel.kt
@@ -0,0 +1,5 @@
+package com.vivek.wo.mvvm.sample.login
+
+class LoginViewModel() {
+
+}
