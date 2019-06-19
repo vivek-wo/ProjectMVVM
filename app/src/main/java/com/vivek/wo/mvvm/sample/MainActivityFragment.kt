e6774275312fcfdfde3be77e72604752bde1387f
vivek-wo <vivek.wei.wu@gmail.com>
2019-06-20 00:10:35 +0800

    基于MVVM的快速开发框架

--- app/src/main/java/com/vivek/wo/mvvm/sample/MainActivityFragment.kt
+++ app/src/main/java/com/vivek/wo/mvvm/sample/MainActivityFragment.kt
@@ -5,11 +5,20 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.vivek.wo.mvvm.sample.databinding.FragmentMainBinding
 
 class MainActivityFragment : Fragment() {
 
+    lateinit var fragmentMainBinding: FragmentMainBinding
+
+    lateinit var mainViewModel: MainViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        fragmentMainBinding = FragmentMainBinding.inflate(inflater, container, false)
+        fragmentMainBinding.viewmodel = mainViewModel
+
+        setHasOptionsMenu(true)
+        return fragmentMainBinding.root
     }
 
     override fun onDestroyView() {
