5f48d29c3d068b020fd589f09bcb6f3d0235504d
vivek-wo <vivek.wei.wu@gmail.com>
2019-06-25 11:21:27 +0800

    基于MVVM的快速开发框架 自定义属性需apply plugin: 'kotlin-kapt'

--- app/src/main/java/com/vivek/wo/mvvm/sample/BindingAdapters.kt
+++ app/src/main/java/com/vivek/wo/mvvm/sample/BindingAdapters.kt
@@ -4,10 +4,11 @@ import android.databinding.BindingAdapter
 import android.widget.ImageView
 
 object BindingAdapters {
+    @BindingAdapter("app:remoteImageUrl")
     @JvmStatic
-    @BindingAdapter("imageUrl")
-    fun setImageUrl(imageView: ImageView, url: String) {
+    fun setRemoteImageUrl(imageView: ImageView, url: String?) {
         println("----------------- $url")
         imageView.setImageResource(R.drawable.ic_launcher_background)
     }
 }
+
